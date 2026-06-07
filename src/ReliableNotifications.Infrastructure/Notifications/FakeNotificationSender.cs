7552459cc7bebb2edb0894e599a03f1074d01ad1
carlosd12c13 <58006644+carlosd12c13@users.noreply.github.com>
2026-06-06 19:41:19 -0600

    feat(infrastructure): implement fake notification sender

---
+++ src/ReliableNotifications.Infrastructure/Notifications/FakeNotificationSender.cs
@@ -0,0 +1,20 @@
+﻿
+using ReliableNotifications.Application.Notifications;
+
+namespace ReliableNotifications.Infrastructure.Notifications;
+
+public sealed class FakeNotificationSender : INotificationSender
+{
+    public async Task<string> SendAsync(
+        string recipient,
+        string message,
+        CancellationToken cancellationToken = default)
+    {
+        await Task.Delay(500, cancellationToken);
+
+        Console.WriteLine(
+            $"[Fake provider] Notification sent to {recipient}: {message}");
+
+        return $"Notification sent to {recipient}";
+    }
+}
