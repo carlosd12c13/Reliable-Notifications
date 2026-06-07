c27db206bea03cfc64776c6419ca66f46f6638ed
carlosd12c13 <58006644+carlosd12c13@users.noreply.github.com>
2026-06-06 19:39:10 -0600

    feat(application): add notification sender port

---
+++ src/ReliableNotifications.Application/Notifications/INotificationSender.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ReliableNotifications.Application.Notifications;
+
+public interface INotificationSender
+{
+    Task<string> SendAsync(
+        string recipient,
+        string message,
+        CancellationToken cancellationToken = default);
+}
